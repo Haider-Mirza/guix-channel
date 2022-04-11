ad4e9b2012cd4f55bc3947f1e59b4af37065514c
Haider Mirza <x7and7@gmail.com>
2022-04-11 15:55:41 +0100

    Btop.scm: My first ever packaged Guix package

---
+++ packages/btop.scm
@@ -0,0 +1,34 @@
+(define-module (btop)
+  #:use-module (guix packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages gcc)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public btop
+  (package
+   (name "btop")
+   (version "1.2.5")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/aristocratos/btop.git")
+           (commit (string-append "v" version))))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "1v0lj296bzwgs29hv9z3r82fwmibiqgsvsqqh2fimxs0jmld7c2v"))))
+
+   (build-system gnu-build-system)
+    (native-inputs
+     (list sed gcc-11))     ; for scripts/MakeHeader.py
+    (arguments
+     '(#:make-flags (list (string-append "PREFIX=" %output))
+       #:tests? #f      ; bats test fails with loading load.bash
+       #:phases (modify-phases %standard-phases (delete 'configure))))
+   (home-page "https://github.com/aristocratos/btop")
+   (synopsis "Linux/OSX/FreeBSD resource monitor")
+   (description
+    "Resource monitor that shows usage and stats for processor, memory, disks, network and processes.")
+   (license license:asl2.0)))
